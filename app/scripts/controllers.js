93cf42380a01fd51a645529981c80deedb39eae5
Liz Abinante <labinante@newrelic.com>
2014-12-30 09:08:57 -0800

    inject app.services; $scope.pattern = forestParkCowl service

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -29,27 +29,6 @@ angular.module('app.controllers', [])
     }
   ];
 }])
-.controller('ForestParkCtrl', ['$scope', function($scope) {
-  $scope.patternDirective = 'forest-park';
-
-  $scope.title = 'Forest Park Cowl';
-  $scope.designer = 'Liz Abinante';
-  $scope.description = 'A simple cabled cowl with garter stitch ribbing.';
-
-  $scope.yarns = ['Approximately 300 yards/274 meters aran (or worsted weight, depending on your gauge) yarn.'];
-
-  $scope.yarnsShown = ['The Plucky Knitter Primo Aran (75% Merino, 20% Cashmere, 5% Nylon); 200 yards/183 meters per 115 grams. Approximately 1.5 skeins used.'];
-
-  $scope.needles = [
-    'One US 7 (4.5mm) 16" circular (or 20" if preferred) needle.'
-  ];
-
-  $scope.gauge = '16 sts, 19 rows per 4"/10cm, garter stitch ribbing.';
-
-  $scope.finishedSizes = [{
-    label: 'one',
-    measurements: 'X"/Ycm tall; X"/Ycm circumference at smallest opening, X"/Ycm circumference at largest opening.'
-  }];
-
-  $scope.notions = ['stitch marker', 'cable needle (optional)', 'darning needle'];
+.controller('ForestParkCtrl', ['$scope', 'app.services', function($scope, 'forestParkCowl') {
+  $scope.pattern = forestParkCowl;
 }]);
