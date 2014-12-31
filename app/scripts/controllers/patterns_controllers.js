572b89154122db5bd4733cdba5ac5f266005def9
Liz Abinante <eabinante@gmail.com>
2014-12-31 13:00:56 -0800

    modal added to pattern pages

--- app/scripts/controllers/patterns_controllers.js
+++ app/scripts/controllers/patterns_controllers.js
@@ -1,9 +1,16 @@
 'use strict';
 
-angular.module('app.patterns_controllers', ['app.patterns_services'])
+angular.module('app.patterns_controllers', ['app.patterns_services', 'ngDialog'])
 .controller('PatternsCtrl', ['$scope', 'patterns', function($scope, patterns) {
   $scope.patterns = patterns.all;
 }])
-.controller('ForestParkCtrl', ['$scope', 'patterns', function($scope, patterns) {
+.controller('ForestParkCtrl', ['$scope', 'patterns', 'ngDialog', function($scope, patterns, ngDialog) {
   $scope.pattern = patterns.forestParkCowl;
+  $scope.clickToOpen = function(img) {
+    ngDialog.open({
+      template: '/partials/modal.html',
+      className: 'ngdialog-theme-default-large',
+      data: img
+    });
+  };
 }]);
