d617fdd0affdb945a1644f4c645ff6f6e2240622
Liz Abinante <eabinante@gmail.com>
2014-12-31 13:34:39 -0800

    decouple custom patterns from patterns to avoid repetition

--- app/scripts/services/custom_patterns_services.js
+++ app/scripts/services/custom_patterns_services.js
@@ -1,14 +1,31 @@
 'use strict';
 
 /* Sevices */
-angular.module('app.custom_patterns_services', ['app.patterns_services'])
-.service('customPatterns', function(patterns) {
+angular.module('app.custom_patterns_services', [])
+.service('customPatterns', function() {
   var veryPdx = function() {
     return {
-      origin: patterns.veryPdx,
-      src: '#/patterns/very-pdx-hat/custom'
+      src: '#/patterns/very-pdx-hat',
+      patternDirective: 'very-pdx',
+      name: 'Very PDX Hat',
+      designer: 'Liz Abinante',
+      customInstructions: 'Knit as-written for a women\'s medium aran weight hat, or customize based on your desired size.',
+      description: 'A unisex cap with a double brim for warmth and smooth finish.',
+      yarns: ['Approximately 135 yards/123.4 meters bulky weight yarn (MC).', 'Approximately 25 yards/22.9 meters aran weight yarn (CC).'],
+      yarnsShown: ['madelinetosh tosh chunky in Steam Age (MC)', 'The Plucky Knitter Primo Aran in Think Pink (CC).'],
+      needles: ['US 8 (5 MM) 16" circular needle (contrast brim).', 'US 9 (5.5 MM) 16" circular needle (brim transition).', 'US 10 (6 MM) 16" circular needle (hat body).', 'US 10 (6 MM) set of DPNs or preferred needles for working small circumferences in the round.'],
+      gauge: 'i am a taco',
+      finishedSizes: [{ label: 'customized', measurements: '13"/33cm - 25"/63.55cm'}, { label: 'as written', measurements: 'women\'s medium (22"/56 cm).' }],
+      notions: ['stitch marker', 'waste yarn', 'darning needle'],
+      version: '1 (December 29, 2014)',
+      images: ['https://s3.amazonaws.com/artcraftcode.com/permanent_assets/very-pdx-1.jpg'],
+      ravelry: {
+        link: '',
+        button: ''
+      },
+      custom: true
     }
-  };
+  }
 
   return {
     all: [veryPdx()],
