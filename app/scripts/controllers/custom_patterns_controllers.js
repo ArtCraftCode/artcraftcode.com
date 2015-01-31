1c8f3ef00c356a174ffad08781cd97bca7f857ef
Liz Abinante <eabinante@gmail.com>
2015-01-31 12:06:41 -0800

    remove custom logic from controller into directive, add broadcast for change

--- app/scripts/controllers/custom_patterns_controllers.js
+++ app/scripts/controllers/custom_patterns_controllers.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('app.custom_patterns_controllers', ['ngDialog', 'app.calculator_services'])
-.controller('CustomCtrl', ['$scope', 'ngDialog', function($scope, ngDialog) {
+.controller('CustomCtrl', ['$scope', '$rootScope', 'ngDialog', function($scope, $rootScope, ngDialog) {
   $scope.customInstructions = false;
 
   $scope.showCustom = function() {
@@ -14,8 +14,7 @@ angular.module('app.custom_patterns_controllers', ['ngDialog', 'app.calculator_s
     dialog.closePromise.then(function(data) {
       if (typeof data.value !== 'string') {
         $scope.customInstructions = true;
-        $scope.customized = true;
-        $scope.customSize = data.value;
+        $rootScope.$broadcast('customized', data.value);
       }
     });
   };
@@ -26,83 +25,4 @@ angular.module('app.custom_patterns_controllers', ['ngDialog', 'app.calculator_s
 }])
 .controller('VeryPdxCtrl', ['$scope', 'customPatterns', 'calculator', function($scope, customPatterns, calculator) {
   $scope.pattern = customPatterns.veryPdx;
-  $scope.customized = false;
-  $scope.customSize = undefined;
-
-  $scope.customize = function(size) {
-    $scope.customSize = size;
-    $scope.customized = true;
-
-    castOn();
-    brimRows();
-    brimHeight();
-    bodyHeight();
-    babyDecreases();
-    decreaseMultiples();
-    yardage();
-
-    $scope.pattern.custom_fields.size_label = $scope.customSize.label;
-    $scope.pattern.finishedSizes = [{label: 'Custom', measurements: $scope.customSize.label}];
-  };
-
-  var castOn = function() {
-    var raw = Math.floor($scope.customSize.value * $scope.pattern.gaugeInch);
-    $scope.pattern.custom_fields.cast_on = calculator.nearestMultiple(raw, $scope.pattern.custom_fields.multiple);
-  };
-
-  var brimRows = function() {
-    $scope.pattern.custom_fields.brim_rows = Math.floor($scope.pattern.gaugeRowInch * $scope.customSize.ribbing);
-  };
-
-  var brimHeight = function() {
-    $scope.pattern.custom_fields.brim_height = $scope.customSize.ribbing;
-  };
-
-  var bodyHeight = function() {
-    if ($scope.pattern.custom_fields.baby_decreases && $scope.pattern.custom_fields.cast_on % 8 !== 0) {
-      $scope.pattern.custom_fields.body_height = $scope.customSize.height - 1;
-    }
-    else if ($scope.pattern.custom_fields.cast_on % 8 !== 0) {
-      $scope.pattern.custom_fields.body_height = $scope.customSize.height - 0.6;
-    } else {
-      $scope.pattern.custom_fields.body_height = $scope.customSize.height;
-    }
-  };
-
-  var babyDecreases = function() {
-    if ($scope.customSize.value < 17) {
-      $scope.pattern.custom_fields.baby_decreases = true;
-    }
-  };
-
-  var decreaseMultiples = function() {
-    // preferred decrease multiple is 8, but I need 4 for sizing
-    if ($scope.pattern.custom_fields.cast_on % 8 !== 0) {
-      $scope.pattern.custom_fields.decrease_4 = true;
-      $scope.pattern.custom_fields.decrease_multiple = $scope.pattern.custom_fields.cast_on/4 - 2;
-    } else {
-      $scope.pattern.custom_fields.decrease_4 = false;
-    }
-  };
-
-  var yardage = function() {
-    var height;
-    if ($scope.pattern.custom_fields.baby_decreases) {
-      height = $scope.pattern.custom_fields.body_height + 1.4;
-    } else {
-      height = $scope.pattern.custom_fields.body_height + 2.2;
-    }
-
-    // main hat
-    var area = calculator.areaHat($scope.customSize.value, height);
-    var yards = Math.floor(calculator.estimateYardage(area, 0.8));
-
-    // contrast brim
-    var ccArea = calculator.areaRectangle($scope.customSize.value, $scope.customSize.ribbing - 0.25);
-    var ccYards = Math.floor(calculator.estimateYardage(ccArea, 0.9));
-    $scope.pattern.yarns = [
-      { label: 'MC', yards: yards, weight: 'aran (or a lighter bulky)' },
-      { label: 'CC', yards: ccYards, weight: 'aran (or a heavy worsted)' }
-    ];
-  }
 }]);
--- app/scripts/directives/patterns_directives.js
+++ app/scripts/directives/patterns_directives.js
@@ -14,23 +14,63 @@ angular.module('app.patterns_directives', ['app.calculator_services'])
     templateUrl: '/partials/winter-sea.html'
   }
 })
-.directive('veryPdx', ['calculator', function(calculator) {
+.directive('veryPdx', ['calculator', 'hatCalculator', function(calculator, hatCalculator) {
   return {
     restrict: 'E',
     scope: {
       custom: '=',
-      customized: '=',
-      customSize: '='
+      pattern: '='
     },
     templateUrl: '/partials/very-pdx.html',
 
     link: function(scope, element, attrs) {
-      // change the value of scope.custom
-      // based on the customSize value
-      if (scope.customized) { customize(); }
+      var decreaseMultiple = 8;
+      var pattern = scope.pattern;
+
+      scope.$on('customized', function(customized, customSize) {
+        scope.customSize = customSize;
+        customize();
+      });
 
       var customize = function() {
+        debugger
+        scope.custom.cast_on = calculator.castOn(scope.customSize.value, pattern.gaugeInch, scope.custom.multiple);
+        scope.custom.brim_rows = hatCalculator.brimRows(pattern.gaugeRowInch, scope.customSize.ribbing);
+        scope.custom.brim_height = scope.customSize.ribbing;
+        scope.custom.body_height = hatCalculator.bodyHeight(scope.custom.cast_on, scope.customSize.height, decreaseMultiple, babyDecreases());
+        scope.custom.size_label = scope.customSize.label;
 
+        // special decreases for non-multiples
+        scope.custom.decrease_4 = false;
+        if (scope.custom.cast_on % decreaseMultiple !== 0) {
+          scope.custom.decrease_4 = true;
+          scope.custom.decrease_mutliple = scope.custom.cast_on/4 - 2;
+        }
+
+        scope.pattern.yarns = [
+          { label: 'MC', yards: mcYards(), weight: 'aran (or a lighter bulky)' },
+          { label: 'CC', yards: ccYards(), weight: 'aran (or a heavy worsed)' },
+        ];
+      };
+
+      // does this size need decreases for babies?
+      var babyDecreases = function() {
+        if (scope.customSize.value < 17) {
+          return true;
+        } else {
+          return false;
+        }
+      };
+
+      var mcYards = function() {
+        var height = babyDecreases() ? scope.custom.body_height + 1.4 : scope.custom.body_height + 2.2;
+        var area = calculator.areaHat(scope.customSize.value, height);
+        return Math.floor(calculator.estimateYardage(area, 0.8));
+      };
+
+      var ccYards = function() {
+        var area = calculator.areaRectangle(scope.customSize.value, scope.customSize.ribbing - 0.25);
+        return Math.floor(calculator.estimateYardage(area, 0.9));
       };
     }
   }
