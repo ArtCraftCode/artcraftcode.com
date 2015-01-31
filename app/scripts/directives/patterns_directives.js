6c441da8f67ca33f996abe444bf1398f01ab9d77
Liz Abinante <eabinante@gmail.com>
2015-01-31 14:51:28 -0800

    extrapolate most of the work for custom hats out of the directive and into the factory

--- app/scripts/directives/patterns_directives.js
+++ app/scripts/directives/patterns_directives.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /* Directives */
-angular.module('app.patterns_directives', ['app.calculator_services'])
+angular.module('app.patterns_directives', ['app.calculator_services', 'factory.custom_pattern', 'library.yarn'])
 .directive('forestPark', function() {
   return {
     restrict: 'E',
@@ -14,7 +14,7 @@ angular.module('app.patterns_directives', ['app.calculator_services'])
     templateUrl: '/partials/winter-sea.html'
   }
 })
-.directive('veryPdx', ['calculator', 'hatCalculator', function(calculator, hatCalculator) {
+.directive('veryPdx', ['calculator', 'hatCalculator', 'CustomHatPattern', 'Yarn', function(calculator, hatCalculator, CustomHatPattern, Yarn) {
   return {
     restrict: 'E',
     scope: {
@@ -24,57 +24,45 @@ angular.module('app.patterns_directives', ['app.calculator_services'])
     templateUrl: '/partials/very-pdx.html',
 
     link: function(scope, element, attrs) {
-      // don't change these or you'll die
-      var decreaseMultiple = 8;
-      var pattern = scope.pattern;
-
       // listen for customization
       scope.$on('customized', function(customized, customSize) {
         scope.customSize = customSize;
         customize();
       });
 
-      // does this size need decreases for babies?
-      scope.babyDecreases = function() {
-        if (!scope.customSize) {
-          return false;
-        } else if (scope.customSize.value < 17) {
-          return true;
-        } else {
-          return false;
-        }
-      };
-
-      // customization functions
+      // generate CustomHatPattern
       var customize = function() {
-        scope.custom.cast_on = calculator.castOn(scope.customSize.value, pattern.gaugeInch, scope.custom.multiple);
-        scope.custom.brim_rows = hatCalculator.brimRows(pattern.gaugeRowInch, scope.customSize.ribbing);
-        scope.custom.brim_height = scope.customSize.ribbing;
-        scope.custom.body_height = hatCalculator.bodyHeight(scope.custom.cast_on, scope.customSize.height, decreaseMultiple, scope.babyDecreases());
-        scope.custom.size_label = scope.customSize.label;
+        var customHat = new CustomHatPattern(scope.customSize, scope.pattern, 8);
 
-        // special decreases for non-multiples
-        scope.custom.decrease_4 = false;
-        if (scope.custom.cast_on % decreaseMultiple !== 0) {
-          scope.custom.decrease_4 = true;
-          scope.custom.decrease_mutliple = scope.custom.cast_on/4 - 2;
+        // baby + odd numbered decreases
+        customHat.set('decrease_4', false);
+        if (customHat.cast_on % customHat.decreaseMultiple !== 0) {
+          customHat.set('decrease_4', true);
         }
 
+        // generate all other values + assign
+        customHat.generate();
+        scope.custom = customHat;
+
+        // yarns
+        var yarn = new Yarn('Aran');
         scope.pattern.yarns = [
-          { label: 'MC', yards: mcYards(), weight: 'aran (or a lighter bulky)' },
-          { label: 'CC', yards: ccYards(), weight: 'aran (or a heavy worsed)' },
+          { label: 'MC', yards: mcYards(), weight: yarn.string() + ' (or a lighter ' + yarn.heavyAlternate().string() + ')' },
+          { label: 'CC', yards: ccYards(), weight: yarn.string() + ' (or a heavy ' + yarn.lightAlternate().string() + ')' },
         ];
       };
 
+      // private - main color yards
       var mcYards = function() {
-        var height = scope.babyDecreases() ? scope.custom.body_height + 1.4 : scope.custom.body_height + 2.2;
+        var height = scope.custom.babyDecreases() ? scope.custom.body_height + 1.4 : scope.custom.body_height + 2.2;
         var area = calculator.areaHat(scope.customSize.value, height);
         return Math.floor(calculator.estimateYardage(area, 0.8));
       };
 
+      // private - contrast color yards
       var ccYards = function() {
-        var area = calculator.areaRectangle(scope.customSize.value, scope.customSize.ribbing - 0.25);
-        return Math.floor(calculator.estimateYardage(area, 0.9));
+        var cArea = calculator.areaRectangle(scope.customSize.value, scope.customSize.ribbing - 0.25);
+        return Math.floor(calculator.estimateYardage(cArea, 0.9));
       };
     }
   }
