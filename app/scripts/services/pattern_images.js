819d33706d3ca6dbdcaca0f950fc693610bf2918
Liz Abinante <eabinante@gmail.com>
2015-02-01 09:18:20 -0800

    misdirected images

--- app/scripts/services/pattern_images.js
+++ app/scripts/services/pattern_images.js
@@ -6,7 +6,12 @@ angular.module('pattern.images', [])
   return {
     forestPark: ['https://s3.amazonaws.com/artcraftcode.com/permanent_assets/forest-park-1.jpg', 'https://s3.amazonaws.com/artcraftcode.com/permanent_assets/forest-park-2.jpg'],
 
-    misdirected: [],
+    misdirected: [
+      'https://s3.amazonaws.com/artcraftcode.com/permanent_assets/misdirected/IMG_6598.JPG',
+      'https://s3.amazonaws.com/artcraftcode.com/permanent_assets/misdirected/IMG_6600.JPG',
+      'https://s3.amazonaws.com/artcraftcode.com/permanent_assets/misdirected/IMG_6602.JPG',
+      'https://s3.amazonaws.com/artcraftcode.com/permanent_assets/misdirected/IMG_6604.JPG'
+      ],
 
     veryPdx: ['https://s3.amazonaws.com/artcraftcode.com/permanent_assets/very-pdx-1.jpg'],
 
