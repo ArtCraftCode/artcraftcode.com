be461063e12041fe9af3ad3753928025ec086f1c
Liz Abinante <eabinante@gmail.com>
2015-02-01 09:31:45 -0800

    this.measurements = []

--- app/scripts/services/pattern_finished_sizes.js
+++ app/scripts/services/pattern_finished_sizes.js
@@ -3,9 +3,16 @@
 /* Sevices */
 angular.module('pattern.finished_sizes', [])
 .factory('FinishedSize', [function() {
-  function FinishedSize(label, measurements) {
+  function FinishedSize(label) {
     this.label = label;
-    this.measurements = measurements;
+    this.measurements = [];
+  }
+
+  FinishedSize.prototype.measurement = function(label, inches) {
+    this.measurements.push({
+      'label': label,
+      'inches': inches
+    });
   }
 
   return (FinishedSize);
