84d8f1095fe8d3e8791e0ee1c957227d411abf0a
Liz Abinante <eabinante@gmail.com>
2015-02-28 14:29:44 -0800

    pattern displaying on index

--- artcraftcode/helpers/pattern_helpers.rb
+++ artcraftcode/helpers/pattern_helpers.rb
@@ -2,8 +2,7 @@ require_relative '../lib/needle.rb'
 require_relative '../lib/patterns/patterns.rb'
 
 module PatternHelpers
-  def patterns
-    pattern = Patterns::FOREST_PARK
-    pattern.designer
+  def pattern
+    Patterns::FOREST_PARK
   end
 end
