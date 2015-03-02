48c9fb27fcdaaa862473864953317260084cf570
Liz Abinante <eabinante@gmail.com>
2015-03-01 17:32:23 -0800

    angular WORKS

---
+++ source/javascripts/controllers.js
@@ -0,0 +1,5 @@
+angular.module('controllers', [])
+.controller('VeryPdxCtrl', ['$scope', function($scope) {
+  $scope.pattern = "hello";
+  console.log("i am a nugget")
+}]);
---
+++ source/javascripts/custom_patterns_app.js
@@ -0,0 +1,13 @@
+//= require "vendor/angular.min"
+//= require "vendor/angular-route.min"
+//= require "vendor/ngDialog.min"
+//= require "controllers"
+
+var App = angular.module('app',
+  [
+    'ngRoute',
+    'ngDialog',
+    'controllers'
+  ]
+);
+
