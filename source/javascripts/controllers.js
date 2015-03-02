91f6a8c54ff3d579cba0d3922d44964eb8b1aa6b
Liz Abinante <eabinante@gmail.com>
2015-03-01 17:51:07 -0800

    very pdx is ruby for now, but angular is wired up - just have to transfer over all of the calculators

--- source/javascripts/controllers.js
+++ source/javascripts/controllers.js
@@ -1,5 +1,9 @@
 angular.module('controllers', [])
 .controller('VeryPdxCtrl', ['$scope', function($scope) {
-  $scope.pattern = "hello";
-  console.log("i am a nugget")
+  $scope.cast_on = 72;
+  $scope.brim_rows = 11;
+  $scope.brim_height = 2;
+  $scope.body_height = 6.5;
+  $scope.multiple = 4;
+  $scope.baby_decreases = false;
 }]);
