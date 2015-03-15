86bb19926555c2fc5f53ebdb3ebf77e1a6a0d1dd
Liz Abinante <eabinante@gmail.com>
2015-03-15 15:06:46 -0700

    remove ngDialog, disqus

--- source/javascripts/custom_patterns_app.js
+++ source/javascripts/custom_patterns_app.js
@@ -1,6 +1,5 @@
 //= require "vendor/angular.min"
 //= require "vendor/angular-route.min"
-//= require "vendor/ngDialog.min"
 //= require "controllers"
 //= require "directives"
 //= require "factories"
@@ -11,7 +10,6 @@
 var App = angular.module('app',
   [
     'ngRoute',
-    'ngDialog',
     'controllers',
     'directives',
     'factories',
