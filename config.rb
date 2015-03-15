f615a4a8daef446a69f235599ba637523c6e772a
Liz Abinante <eabinante@gmail.com>
2015-03-15 04:53:17 -0700

    clean up file, move test settings to build

--- config.rb
+++ config.rb
@@ -5,15 +5,6 @@ set :markdown, :fenced_code_blocks => true, :smartypants => true
 activate :directory_indexes
 
 ###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
 # Page options, layouts, aliases and proxies
 ###
 
@@ -30,49 +21,22 @@ activate :directory_indexes
 #   page "/admin/*"
 # end
 
-# Proxy pages (https://middlemanapp.com/advanced/dynamic_pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
-
-###
-# Helpers
-###
-
 # Automatic image dimensions on image_tag helper
 # activate :automatic_image_sizes
 
-# Reload the browser automatically whenever files change
 configure :development do
   activate :livereload
 end
 
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
-set :css_dir, 'test/stylesheets'
-
-set :js_dir, 'test/javascripts'
-
+set :css_dir, 'stylesheets'
+set :js_dir, 'javascripts'
 set :images_dir, 'test/images'
 
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
   activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
-
-  # Enable cache buster
-  # activate :asset_hash
-
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
+  # test environment
+  # set :css_dir, 'test/stylesheets'
+  # set :js_dir, 'test/javascripts'
+  # set :images_dir, 'test/images'
 end
